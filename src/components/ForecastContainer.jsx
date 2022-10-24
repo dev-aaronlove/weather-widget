45df420341386ded2e34c8befba7a6e060665e7d
Aaron Love <dev.aaronlove@gmail.com>
2022-10-24 08:47:42 -0600

    Create env variable and filter response data

--- src/components/ForecastContainer.jsx
+++ src/components/ForecastContainer.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import DayCard from "./DayCard";
 import DegreeToggle from "./DegreeToggle";
+import {WEATHER_URL, WEATHER_API} from "../constants";
 
 class ForecastContainer extends React.Component {
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(`${WEATHER_URL}59d4c6fc6e13eaab4d9b6f5e00285021`);
+      if (response.ok) {
+
+        const json = await response.json();
+        const data = await json.list
+        .filter(day => day.dt_txt.includes("00:00:00"))
+        .map(item => ({
+          temp: item.main.temp,
+          dt: item.dt,
+          date: item.dt_txt,
+          imgId: item.weather[0].icon,
+          desc: item.weather[0].description
+        }));
+        console.log('data: ', data);
+      } else console.log('response is not ok');
+    } catch(err) {
+      console.log("There was an error: ", err);
+    }
+  }
 
   render() {
     return (
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?zip=84096&units=imperial&appid=";
+export const WEATHER_API = process.env.REACT_APP_WEATHER_API;
+// process.env --> access .env variables
